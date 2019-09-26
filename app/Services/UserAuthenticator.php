b6054cfa39c0dcb9de22c7aaa1f2b5571db8db66
Chris Thomas <chris.thomas@antimatter-studios.com>
2019-09-26 19:02:05 +0200

    throw 400 bad request when validation fails and specify the reason why it failed

--- app/Services/UserAuthenticator.php
+++ app/Services/UserAuthenticator.php
@@ -7,6 +7,7 @@ use App\Model\UserToken;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class UserAuthenticator
@@ -30,44 +31,52 @@ class UserAuthenticator
         // TODO: This could request the metadata service to return a list of registered services?
         $repositoryTypes = ['php'];
 
-        $validator = Validator::make($headers, [
-            'reporangler-login-type' => 'string|in:'.implode(',', $loginTypes),
-            'reporangler-login-username' => 'required|string',
-            'reporangler-login-password' => 'required|string',
-            'reporangler-login-repository-type' => 'string|in:'.implode(',', $repositoryTypes),
-        ]);
+        try{
+            $validator = Validator::make($headers, [
+                'reporangler-login-type' => 'string|in:'.implode(',', $loginTypes),
+                'reporangler-login-username' => 'required|string',
+                'reporangler-login-password' => 'required|string',
+                'reporangler-login-repository-type' => 'string|in:'.implode(',', $repositoryTypes),
+            ]);
 
-        $data = $validator->validate();
+            $data = $validator->validate();
 
-        // We override this because we always want to check the database in this case
-        if($data['reporangler-login-type'] === 'http-basic'){
-            $data['reporangler-login-type'] = 'database';
+            // We override this because we always want to check the database in this case
+            if($data['reporangler-login-type'] === 'http-basic'){
+                $data['reporangler-login-type'] = 'database';
+            }
+
+            // Default to database logins
+            if(!array_key_exists('reporangler-login-type', $data)){
+                $data['reporangler-login-type'] = 'database';
+            }
+
+            // Default to REST Api User Login
+            if(!array_key_exists('reporangler-login-repository-type', $data)){
+                $data['reporangler-login-repository-type'] = null;
+            }
+
+            return $data;
+        }catch(ValidationException $e){
+            abort(400, 'Headers were invalid');
         }
-
-        // Default to database logins
-        if(!array_key_exists('reporangler-login-type', $data)){
-            $data['reporangler-login-type'] = 'database';
-        }
-
-        // Default to REST Api User Login
-        if(!array_key_exists('reporangler-login-repository-type', $data)){
-            $data['reporangler-login-repository-type'] = null;
-        }
-
-        return $data;
     }
 
     public function validateTokenRequest(Request $request)
     {
         $headers = $this->flattenHeaders($request->headers->all());
 
-        $validator = Validator::make($headers, [
-            'authorization' => 'required|string'
-        ]);
+        try {
+            $validator = Validator::make($headers, [
+                'authorization' => 'required|string'
+            ]);
 
-        $data = $validator->validate();
+            $data = $validator->validate();
 
-        return $data['authorization'];
+            return $data['authorization'];
+        }catch(ValidationException $e) {
+            abort(400, 'Headers were invalid');
+        }
     }
 
     public function login(string $type, string $username, string $password): User
