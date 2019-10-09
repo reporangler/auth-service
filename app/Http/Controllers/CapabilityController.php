e0c23371fe15abf56d900ebce577bc30a5db31f7
Chris Thomas <chris.thomas@antimatter-studios.com>
2019-10-10 00:34:36 +0200

    implement what I think is the basis for adding and remove users from package groups and repositories.

--- app/Http/Controllers/CapabilityController.php
+++ app/Http/Controllers/CapabilityController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Meta\UserToPackageGroup;
+use App\Meta\UserToRepository;
 use App\Model\Capability;
 use App\Model\PackageGroup;
 use App\Model\User;
 use App\Model\UserCapability;
-use App\Model\UserPackageGroup;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Laravel\Lumen\Routing\Controller as BaseController;
+use RepoRangler\Entity\Repository;
 
 class CapabilityController extends BaseController
 {
@@ -28,32 +30,16 @@ class CapabilityController extends BaseController
         $created = [];
         $exists = [];
 
-        $capability = UserPackageGroup::whereUserHasPackageGroup($user, $packageGroup, Capability::PACKAGE_GROUP_ACCESS)->first();
+        $capability = UserToPackageGroup::where($user, $packageGroup, Capability::PACKAGE_GROUP_ACCESS)->first();
         if($capability === null){
-            $capability = new UserCapability([
-                'name' => Capability::PACKAGE_GROUP_ACCESS,
-                'user_id' => $user->id,
-                'constraint' => [
-                    'name' => $packageGroup->name
-                ]
-            ]);
-            $capability->save();
-            $created[] = $capability;
+            $created[] = UserToPackageGroup::create(Capability::PACKAGE_GROUP_ACCESS, $user, $packageGroup);
         }else{
             $exists[] = $capability;
         }
 
-        $capability = UserPackageGroup::whereUserHasPackageGroup($user, $packageGroup, Capability::PACKAGE_GROUP_ADMIN)->first();
+        $capability = UserToPackageGroup::where($user, $packageGroup, Capability::PACKAGE_GROUP_ADMIN)->first();
         if($capability === null){
-            $capability = new UserCapability([
-                'name' => Capability::PACKAGE_GROUP_ADMIN,
-                'user_id' => $user->id,
-                'constraint' => [
-                    'name' => $packageGroup->name
-                ]
-            ]);
-            $capability->save();
-            $created[] = $capability;
+            $created[] = UserToPackageGroup::create(Capability::PACKAGE_GROUP_ADMIN, $user, $packageGroup);
         }else{
             $exists[] = $capability;
         }
@@ -63,46 +49,110 @@ class CapabilityController extends BaseController
 
     public function leavePackageGroup(Request $request)
     {
-        return new JsonResponse(['method' => __METHOD__]);
+        $data = $this->validate($request, [
+            'user_id' => 'required|integer|min:1',
+            'package_group_id' => 'required|integer|min:1',
+        ]);
+
+        $user = User::findOrFail($data['user_id']);
+        $packageGroup = PackageGroup::findOrFail($data['package_group_id']);
+
+        $deleted = [];
+
+        $tests = [Capability::PACKAGE_GROUP_ACCESS, Capability::PACKAGE_GROUP_ADMIN];
+
+        foreach($tests as $permission){
+            $capability = UserToPackageGroup::where($user, $packageGroup, $permission)->first();
+            if($capability instanceOf UserCapability) {
+                $deleted[] = $capability->toArray();
+                $capability->delete();
+            }
+        }
+
+        return new JsonResponse(['deleted' => $deleted]);
     }
 
     public function requestJoinPackageGroup(Request $request)
     {
-        return new JsonResponse(['method' => __METHOD__]);
+        return new JsonResponse(['method' => __METHOD__, 'todo' => 'at the moment only users can have permissions']);
     }
 
     public function joinRepository(Request $request)
     {
-        return new JsonResponse(['method' => __METHOD__]);
+        $data = $this->validate($request, [
+            'user_id' => 'required|integer|min:1',
+            'repository_id' => 'required|string',
+            'access' => 'boolean',
+            'admin' => 'boolean',
+        ]);
+
+        $user = User::findOrFail($data['user_id']);
+        $repository = new Repository(['name' => $data['repository_id']]);
+
+        $created = [];
+        $exists = [];
+
+        $tests = [Capability::REPOSITORY_ACCESS, Capability::REPOSITORY_ADMIN];
+
+        foreach($tests as $permission){
+            $capability = UserToRepository::where($user, $repository, $permission)->first();
+            if($capability === null){
+                $created[] = UserToRepository::create($permission, $user, $repository);
+            }else{
+                $exists[] = $capability;
+            }
+        }
+
+        return new JsonResponse(['created' => $created, 'exists' => $exists]);
     }
 
     public function leaveRepository(Request $request)
     {
-        return new JsonResponse(['method' => __METHOD__]);
+        $data = $this->validate($request, [
+            'user_id' => 'required|integer|min:1',
+            'repository_id' => 'required|string',
+        ]);
+
+        $user = User::findOrFail($data['user_id']);
+        $repository = new Repository(['name' => $data['repository_id']]);
+
+        $deleted = [];
+
+        $tests = [Capability::REPOSITORY_ACCESS, Capability::REPOSITORY_ADMIN];
+
+        foreach($tests as $permission){
+            $capability = UserToRepository::where($user, $repository, $permission)->first();
+            if($capability instanceOf UserCapability) {
+                $deleted[] = $capability->toArray();
+                $capability->delete();
+            }
+        }
+
+        return new JsonResponse(['deleted' => $deleted]);
     }
 
     public function requestJoinRepository(Request $request)
     {
-        return new JsonResponse(['method' => __METHOD__]);
+        return new JsonResponse(['method' => __METHOD__, 'todo' => 'at the moment only users can have permissions']);
     }
 
     public function protectPackageGroup(Request $request)
     {
-        return new JsonResponse(['method' => __METHOD__]);
+        return new JsonResponse(['method' => __METHOD__, 'todo' => 'at the moment only users can have permissions']);
     }
 
     public function unprotectPackageGroup(Request $request)
     {
-        return new JsonResponse(['method' => __METHOD__]);
+        return new JsonResponse(['method' => __METHOD__, 'todo' => 'at the moment only users can have permissions']);
     }
 
     public function protectRepository(Request $request)
     {
-        return new JsonResponse(['method' => __METHOD__]);
+        return new JsonResponse(['method' => __METHOD__, 'todo' => 'at the moment only users can have permissions']);
     }
 
     public function unprotectRepository(Request $request)
     {
-        return new JsonResponse(['method' => __METHOD__]);
+        return new JsonResponse(['method' => __METHOD__, 'todo' => 'at the moment only users can have permissions']);
     }
 }
---
+++ app/Meta/UserToPackageGroup.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Meta;
+
+use App\Model\Capability;
+use App\Model\PackageGroup;
+use App\Model\User;
+use App\Model\UserCapability;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+
+class UserToPackageGroup
+{
+    static public function create(string $cap, User $user, PackageGroup $packageGroup): UserCapability
+    {
+        $capability = new UserCapability([
+            'name' => $cap,
+            'user_id' => $user->id,
+            'constraint' => [
+                'name' => $packageGroup->name
+            ]
+        ]);
+        $capability->save();
+
+        return $capability;
+    }
+
+    static public function where(User $user, PackageGroup $packageGroup, string $access = null)
+    {
+        $access = $access === null
+            ? [Capability::PACKAGE_GROUP_ADMIN, Capability::PACKAGE_GROUP_ACCESS]
+            : [$access];
+
+        return UserCapability::whereHas('capability', function (Builder $query) use ($access){
+            $query->whereIn('name', $access);
+        })->where([
+            'user_id' => $user->id,
+            'constraint->name' => $packageGroup->name,
+        ]);
+    }
+}
