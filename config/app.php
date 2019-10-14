3c3ea98fb96b9ed806d5c055adbb16636456fdf6
Chris Thomas <chris.thomas@antimatter-studios.com>
2019-10-15 00:19:07 +0200

    The default configuration of all these urls was not really very well put, so I'll set them better here before I think of something better

--- config/app.php
+++ config/app.php
@@ -11,8 +11,13 @@ return [
     'debug' => env('APP_DEBUG', false),
     'protocol' => $required['APP_PROTOCOL'],
     'domain' => env('APP_DOMAIN', $required['APP_DOMAIN']),
-    'auth_base_url' => env('APP_URL', "{$required['APP_PROTOCOL']}://{$required['APP_NAME']}.{$required['APP_DOMAIN']}"),
 
     // The number of hours a token can live before being erased
     'token_life_hours' => 6,
+
+    'php_base_url'      => env('APP_PHP_URL',       "{$required['APP_PROTOCOL']}://php.{$required['APP_DOMAIN']}"),
+    'npm_base_url'      => env('APP_NPM_URL',       "{$required['APP_PROTOCOL']}://npm.{$required['APP_DOMAIN']}"),
+    'auth_base_url'     => env('APP_AUTH_URL',      "{$required['APP_PROTOCOL']}://auth.{$required['APP_DOMAIN']}"),
+    'metadata_base_url' => env('APP_METADATA_URL',  "{$required['APP_PROTOCOL']}://metadata.{$required['APP_DOMAIN']}"),
+    'storage_base_url'  => env('APP_STORAGE_URL',   "{$required['APP_PROTOCOL']}://storage.{$required['APP_DOMAIN']}"),
 ];
