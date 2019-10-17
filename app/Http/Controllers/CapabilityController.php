3efb954d2f2609beb564b0390759f84dba275a98
Chris Thomas <chris.thomas@antimatter-studios.com>
2019-10-18 00:51:51 +0200

    refactor the code to be a bit simpler and fix a bug with the permissions were giving admin in the opposite condition that they needed to

--- app/Http/Controllers/CapabilityController.php
+++ app/Http/Controllers/CapabilityController.php
@@ -35,17 +35,14 @@ class CapabilityController extends BaseController
         $created = [];
         $exists = [];
 
-        $capability = UserToPackageGroup::where($user, $packageGroup, $repository, Capability::PACKAGE_GROUP_ACCESS)->first();
-        if($capability === null){
-            $created[] = UserToPackageGroup::create(Capability::PACKAGE_GROUP_ACCESS, $user, $packageGroup, $repository);
-        }else{
-            $exists[] = $capability;
-        }
+        $permissions = array_key_exists('admin', $data) && $data['admin'] === true
+            ? [Capability::PACKAGE_GROUP_ACCESS, Capability::PACKAGE_GROUP_ADMIN]
+            : [Capability::PACKAGE_GROUP_ACCESS];
 
-        if(array_key_exists('admin', $data) && $data['admin'] === true){
-            $capability = UserToPackageGroup::where($user, $packageGroup, $repository, Capability::PACKAGE_GROUP_ADMIN)->first();
+        foreach($permissions as $p){
+            $capability = UserToPackageGroup::where($user, $packageGroup, $repository, $p)->first();
             if($capability === null){
-                $created[] = UserToPackageGroup::create(Capability::PACKAGE_GROUP_ADMIN, $user, $packageGroup, $repository);
+                $created[] = UserToPackageGroup::create($p, $user, $packageGroup, $repository);
             }else{
                 $exists[] = $capability;
             }
@@ -70,10 +67,10 @@ class CapabilityController extends BaseController
 
         $deleted = [];
 
-        $tests = [Capability::PACKAGE_GROUP_ACCESS, Capability::PACKAGE_GROUP_ADMIN];
+        $permissions = [Capability::PACKAGE_GROUP_ACCESS, Capability::PACKAGE_GROUP_ADMIN];
 
-        foreach($tests as $permission){
-            $capability = UserToPackageGroup::where($user, $packageGroup, $repository, $permission)->first();
+        foreach($permissions as $p){
+            $capability = UserToPackageGroup::where($user, $packageGroup, $repository, $p)->first();
             if($capability instanceOf UserCapability) {
                 $deleted[] = $capability->toArray();
                 $capability->delete();
@@ -83,32 +80,36 @@ class CapabilityController extends BaseController
         return new JsonResponse(['deleted' => $deleted]);
     }
 
-    public function requestJoinPackageGroup(Request $request)
+    public function requestJoinPackageGroup(Request $request, MetadataClient $metadataClient)
     {
         return new JsonResponse(['method' => __METHOD__, 'todo' => 'at the moment only users can have permissions']);
     }
 
-    public function joinRepository(Request $request)
+    public function joinRepository(Request $request, MetadataClient $metadataClient)
     {
+        $login = Auth::guard('token')->user();
+
         $data = $this->validate($request, [
             'user_id' => 'required|integer|min:1',
-            'repository_id' => 'required|string',
-            'access' => 'boolean',
-            'admin' => 'boolean',
+            'repository_id' => 'required|integer|min:1',
+            'access' => 'required_without:admin|boolean',
+            'admin' => 'required_without:access|boolean',
         ]);
 
         $user = User::findOrFail($data['user_id']);
-        $repository = new Repository(['name' => $data['repository_id']]);
+        $repository = $metadataClient->getRepositoryById($login->token, $data['repository_id']);
 
         $created = [];
         $exists = [];
 
-        $tests = [Capability::REPOSITORY_ACCESS, Capability::REPOSITORY_ADMIN];
+        $permissions = array_key_exists('admin', $data) && $data['admin'] === true
+            ? [Capability::REPOSITORY_ACCESS, Capability::REPOSITORY_ADMIN]
+            : [Capability::REPOSITORY_ACCESS];
 
-        foreach($tests as $permission){
-            $capability = UserToRepository::where($user, $repository, $permission)->first();
+        foreach($permissions as $p){
+            $capability = UserToRepository::where($user, $repository, $p)->first();
             if($capability === null){
-                $created[] = UserToRepository::create($permission, $user, $repository);
+                $created[] = UserToRepository::create($p, $user, $repository);
             }else{
                 $exists[] = $capability;
             }
@@ -117,22 +118,24 @@ class CapabilityController extends BaseController
         return new JsonResponse(['created' => $created, 'exists' => $exists]);
     }
 
-    public function leaveRepository(Request $request)
+    public function leaveRepository(Request $request, MetadataClient $metadataClient)
     {
+        $login = Auth::guard('token')->user();
+
         $data = $this->validate($request, [
             'user_id' => 'required|integer|min:1',
-            'repository_id' => 'required|string',
+            'repository_id' => 'required|integer|min:1',
         ]);
 
         $user = User::findOrFail($data['user_id']);
-        $repository = new Repository(['name' => $data['repository_id']]);
+        $repository = $metadataClient->getRepositoryById($login->token, $data['repository_id']);
 
         $deleted = [];
 
-        $tests = [Capability::REPOSITORY_ACCESS, Capability::REPOSITORY_ADMIN];
+        $permissions = [Capability::REPOSITORY_ACCESS, Capability::REPOSITORY_ADMIN];
 
-        foreach($tests as $permission){
-            $capability = UserToRepository::where($user, $repository, $permission)->first();
+        foreach($permissions as $p){
+            $capability = UserToRepository::where($user, $repository, $p)->first();
             if($capability instanceOf UserCapability) {
                 $deleted[] = $capability->toArray();
                 $capability->delete();
@@ -142,27 +145,27 @@ class CapabilityController extends BaseController
         return new JsonResponse(['deleted' => $deleted]);
     }
 
-    public function requestJoinRepository(Request $request)
+    public function requestJoinRepository(Request $request, MetadataClient $metadataClient)
     {
         return new JsonResponse(['method' => __METHOD__, 'todo' => 'at the moment only users can have permissions']);
     }
 
-    public function protectPackageGroup(Request $request)
+    public function protectPackageGroup(Request $request, MetadataClient $metadataClient)
     {
         return new JsonResponse(['method' => __METHOD__, 'todo' => 'at the moment only users can have permissions']);
     }
 
-    public function unprotectPackageGroup(Request $request)
+    public function unprotectPackageGroup(Request $request, MetadataClient $metadataClient)
     {
         return new JsonResponse(['method' => __METHOD__, 'todo' => 'at the moment only users can have permissions']);
     }
 
-    public function protectRepository(Request $request)
+    public function protectRepository(Request $request, MetadataClient $metadataClient)
     {
         return new JsonResponse(['method' => __METHOD__, 'todo' => 'at the moment only users can have permissions']);
     }
 
-    public function unprotectRepository(Request $request)
+    public function unprotectRepository(Request $request, MetadataClient $metadataClient)
     {
         return new JsonResponse(['method' => __METHOD__, 'todo' => 'at the moment only users can have permissions']);
     }
